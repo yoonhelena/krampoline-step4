320880b92c11bf1e17c437f1f50230d3d4ad475e
hostdog01 <hostdog01@naver.com>
2023-08-22 15:08:51 +0000

    db update

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,2 @@
 # MariaDB의 최신 버전을 기반으로 합니다.
 FROM mariadb:latest
-
-# schema.sql 및 data.sql 파일을 이미지에 추가합니다.
-ADD schema.sql /docker-entrypoint-initdb.d
-ADD data.sql /docker-entrypoint-initdb.d
--- data.sql
+++ data.sql
@@ -1,7 +1,7 @@
 use `krampoline`;
 
 GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;
-GRANT ALL ON kakao.* TO 'root'@'localhost';
+GRANT ALL ON krampoline.* TO 'root'@'localhost';
 FLUSH PRIVILEGES;
 
 INSERT INTO test_table (`id`,`detail`) VALUES ('1', 'Hello DKOS!');
