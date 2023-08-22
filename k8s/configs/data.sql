a576b9dedfc209dc7f474db94b63b0ce455d9d89
hostdog01 <hostdog01@naver.com>
2023-08-22 16:32:57 +0000

    mariadb test

--- k8s/configs/data.sql
+++
@@ -1,7 +0,0 @@
-use `krampoline`;
-
-GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;
-GRANT ALL ON krampoline.* TO 'root'@'localhost';
-FLUSH PRIVILEGES;
-
-INSERT INTO sample_data (`id`,`detail`) VALUES ('1', 'Hello DKOS!');
