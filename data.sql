abb79f995f161f605fabe12944ed4b6d644e0868
hostdog01 <hostdog01@naver.com>
2023-08-22 15:33:58 +0000

    data

--- data.sql
+++ data.sql
@@ -4,4 +4,4 @@ GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;
 GRANT ALL ON krampoline.* TO 'root'@'localhost';
 FLUSH PRIVILEGES;
 
-INSERT INTO test_table (`id`,`detail`) VALUES ('1', 'Hello DKOS!');
+INSERT INTO sample_data (`id`,`detail`) VALUES ('1', 'Hello DKOS!');
--- schema.sql
+++ schema.sql
@@ -2,10 +2,9 @@ CREATE SCHEMA IF NOT EXISTS `krampoline` DEFAULT CHARACTER SET utf8mb4;
 
 USE `krampoline`;
 
-
 DROP TABLE IF EXISTS `sample_data`;
 CREATE TABLE `sample_data` (
-                           `id` int(11) NOT NULL AUTO_INCREMENT,
-                           `detail` varchar(100) NOT NULL,
-                           PRIMARY KEY (`id`),
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `detail` varchar(100) NOT NULL,
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
